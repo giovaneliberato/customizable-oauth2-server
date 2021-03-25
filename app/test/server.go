b201cf78313dff57a4ee99992f12f7422f765f33
Giovane Liberato <giovaneliberato@gmail.com>
2021-03-25 20:14:45 -0300

    create test config file

--- app/test/server.go
+++ app/test/server.go
@@ -4,13 +4,18 @@ import (
 	"goauth-extension/app/domain/client"
 	"goauth-extension/app/infra"
 	"net/http/httptest"
+	"path"
+	"path/filepath"
+	"runtime"
 
 	"github.com/go-chi/chi"
 	"github.com/golobby/container/v2"
+	"github.com/spf13/viper"
 )
 
 func ConfigureTestScenario() {
-	infra.InitApplication()
+	infra.InitializeComponents()
+	loadTestConfigFile()
 
 	var repository client.Repository
 	container.Make(&repository)
@@ -27,4 +32,21 @@ func TestServerFor(routes ...func(*chi.Mux)) *httptest.Server {
 	}
 
 	return server
+}
+
+func loadTestConfigFile() {
+	viper.AddConfigPath(rootDir() + "/test")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
 }
